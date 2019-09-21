4d9415c18b2a7703446560640c2eed3690faab60
최은정 <morned270@daum.net>
2019-09-22 05:15:50 +0900

    first upload!

---
+++ prac2_jeong/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿/* ************************************************
+ * 프로그램명 : HelloWorld.cs 
+ * 작성자 : 2018038044 최은정
+ * 작성일 : 2019.09.19
+ * 프로그램설명 : 실습 1_수식계산프로그램
+ * ************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            int FirstNum;
+            int SecondNum;
+            string operatorr;
+            int result = 0;
+
+            Console.Write("첫 번째 숫자를 입력하세요        : ");
+            FirstNum = int.Parse(Console.ReadLine());
+
+            Console.Write("연산자를 입력하세요 (+, -, *, /) : ");
+            operatorr = Console.ReadLine();
+
+            Console.Write("두 번째 숫자를 입력하세요        : ");
+            SecondNum = int.Parse(Console.ReadLine());
+
+            //switch문
+            switch (operatorr)
+            {
+                case "+":
+                    result = FirstNum + SecondNum;
+                    break;
+
+                case "-":
+                    result = FirstNum - SecondNum;
+                    break;
+
+                case "*":
+                    result = FirstNum * SecondNum;
+                    break;
+
+                case "/":
+                    result = FirstNum / SecondNum;
+                    break;
+            }
+
+            Console.WriteLine("연산 결과는 다음과 같습니다      : {0} {1} {2} = {3}\n", FirstNum, operatorr, SecondNum, result);
+
+        }
+    }
+}
---
+++ prac2_jeong/ConsoleApp1/클래스_실습2.cs
@@ -0,0 +1,47 @@
+﻿/* ************************************************
+ * 프로그램명 : 클래스_실습2.cs
+ * 작성자 : 2018038044 최은정
+ * 작성일 : 2019.09.19
+ * 프로그램설명 : 실습 2_성적계산 프로그램
+ * ************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1{
+    class Student{
+        private int kor;
+        private int math;
+        private int eng;
+        private double average;
+
+        public void SetScore()
+        {
+            Console.Write("국어 성적 : ");
+            this.kor = int.Parse(Console.ReadLine());
+            Console.Write("수학 성적 : ");
+            this.math = int.Parse(Console.ReadLine());
+            Console.Write("영어 성적 : ");
+            this.eng = int.Parse(Console.ReadLine());
+
+        }
+        public double GetAverage()
+        {
+            average = (double)((this.kor + this.math + this.eng) / 3);
+            return this.average;
+        }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Student jeong = new Student();  //jeong 학생 객체 생성
+            
+            jeong.SetScore();
+            
+            Console.WriteLine("과목 평균 : {0:f2}\n", jeong.GetAverage());
+
+        }
+    }
+}
